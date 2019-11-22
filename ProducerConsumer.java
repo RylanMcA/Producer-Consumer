43f603620bfde28ec5f5af2903c5079f4b1ad107
RylanMcA <55506412+RylanMcA@users.noreply.github.com>
2019-11-21 18:08:31 -0600

    Massive Update
    Should be working?

--- ProducerConsumer.java
+++ ProducerConsumer.java
@@ -3,34 +3,52 @@
 
 import java.util.concurrent.Semaphore;
 import java.util.Random;
-
+ 
 //BUFFER CLASS 
 class Buffer{
+    //Declare needed buffer varables 
     final int BUFFER_SIZE = 5;
     int[] buffer;
+    int currentPos = 0; //How many slots are open in the buffer
+    int outPos = 0; //Position of what comes out next
+    int inPos = 0; //Position of what comes in next.
 
+    //Create semaphores
     Semaphore full = new Semaphore(5);
     Semaphore empty = new Semaphore(0);
     Semaphore mutex = new Semaphore(1);
 
-    public Buffer(){
+    public Buffer(){ //Create the bounded buffer array.
         this.buffer = new int[this.BUFFER_SIZE];
-
     }
 
+    //Adds an item to the buffer
     int insert(int item){
+        while(currentPos == BUFFER_SIZE){} //No need to do anything if we're full
 
+        //if we're not full
+        currentPos++;
+        buffer[inPos] = item;
+        inPos = (inPos+1) % BUFFER_SIZE;
+        System.out.println("Producer Produced: "+ item);
         return 0;
     }
 
-    int remove(int item){
-
-        return 0;
+    //Removes an item form the buffer
+    int remove(){
+        while(currentPos == 0){} //No need to do anything if we're empty.
+        
+        //if we're not empty...
+        currentPos--;
+        int consumed = buffer[outPos];
+        outPos = (outPos+1) % BUFFER_SIZE;
+        System.out.println("Consumer consumed: "+consumed);
+        return consumed;
     }
 
 }
 
-// PRODUCER CLASS - PUTS NUMBERS INTO THING
+// PRODUCER CLASS - PUTS NUMBERS INTO BUFFER
 class Producer extends Thread{
     Random randSleep = new Random();
     Random randInsert = new Random();
@@ -38,43 +56,66 @@ class Producer extends Thread{
 
     public Producer(Buffer buffer){
         this.b = buffer;
-        while(true){
-            try{    
-                Thread.sleep(randSleep.nextInt(500)); 
-            }
-            catch(InterruptedException e){}
-            int item = randInsert.nextInt();
-            if(b.insert(item) == 0){
-                System.out.println("Producer Produced "+item+".");
-            } else {
-                System.out.println("Producer error");
+    }
+
+    public void run(){
+        for(int i=0; i<100;i++){
+            try{ 
+                Thread.sleep(randSleep.nextInt(501)); 
+            } catch(InterruptedException e){
+                System.out.println("Error in Producer sleeping");
             }
 
-        }
-    }
+            int item = randInsert.nextInt();
+
+            try{ //Aquire empty sempahore and mutex
+                b.empty.acquire();
+                b.mutex.acquire();
+            } catch(InterruptedException e){
+                System.out.println("Failed to acquire empty/mutex in Producer class");
+            }
+
+            b.insert(item);
+            b.mutex.release();
+            b.full.release();
+
+
+        } //End of the loop
+    } //End of run
+
 }
 
-//CONSUMER CLASS - TAKES NUMBERS OUT OF THING
+//CONSUMER CLASS - TAKES NUMBERS OUT OF BUFFER
 class Consumer extends Thread{
     Random rand = new Random();
     Buffer b;
 
     public Consumer(Buffer buffer){
         this.b = buffer;
-        while(true){
-            try{    
-                Thread.sleep(rand.nextInt(500)); 
-            }
-            catch(InterruptedException e){}
-            int item = b.remove(0);
-            if(item == 0){
-                System.out.println("Consumer Consumed"+ item);
-            } else {
-                System.out.println("Consumer error");
+    }
+
+    public void run(){
+        for(int i = 0;i<100;i++){
+            try{
+                Thread.sleep(rand.nextInt(501));
+            }catch(InterruptedException e){
+                System.out.println("Exception in Consumer Sleeping");
             }
 
-        }
-    }
+            try{
+                b.full.acquire();
+                b.mutex.acquire();
+            } catch (InterruptedException e){
+                System.out.println("Exception in acquireing full/mutex in Consumer");
+            }
+
+            b.remove();
+            b.mutex.release();
+            b.full.release();
+
+        } //End of loop
+ 
+    }//End of run
 }
 
 
